0809c6e253e295de32f8efe69f77abea34863ccd
Sky Team <theskyteam@live.com>
2011-10-26 11:47:36 +0700

    second commit

---
+++ FormHost/FormHost/IService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace FormHost
+{
+
+    [ServiceContract(Namespace = "http://myuri.org/Simple")]
+    public interface IService
+    {
+        [OperationContract]
+        string LogIn();
+    }
+}
---
+++ FormHost/FormHost/Service.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace FormHost
+{
+    [ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+    public class Service : IService
+    {
+        public string LogIn()
+        {
+            return "YOU HAVE LOGGED IN";
+        }
+    }
+}

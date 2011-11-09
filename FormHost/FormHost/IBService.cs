37475e0a36dca889b6b186ff3052c4b77062c3cb
Sky Team <theskyteam@live.com>
2011-11-10 00:11:52 +0700

    Week 09

---
+++ FormHost/FormHost/IBService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Service
+{
+    [ServiceContract]
+    public interface IBService
+    {
+        [OperationContract]
+        string LogIn();
+    }
+}
